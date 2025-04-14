8bff9a2329e2d451c3987fcfefc2f7dbf0b23fb0
ZT-CONG <zhitao.cong@outlook.com>
2025-04-14 20:41:53 +0800

    Update openai_helper.py

--- bot/openai_helper.py
+++ bot/openai_helper.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 import datetime
 import logging
 import os
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 import tiktoken
 
